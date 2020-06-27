bb5cd8a0bd7b2358c808885d17e0911deba2a550
lhttq <lhttq19970721@163.com>
2020-06-27 18:05:35 +0800

    1.0

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+      <el-card class="box-card">
+          <router-view></router-view>
+      </el-card>
+        </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 30vw;
+  height: 50vh;
+  text-align: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  color: #2c3e50;
+}
+</style>
